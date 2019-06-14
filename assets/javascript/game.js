809ae935252e742d8b8b778837b847613336e4a0
michael mariea <mariea.michael@gmail.com>
2019-06-13 17:58:06 -0700

    fixed the NaN issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,53 +19,52 @@ $('#pic2').attr('data-value1', crystal2v);
 $('#pic3').attr('data-value1', crystal3v);
 $('#pic4').attr('data-value1', crystal4v);
 
-console.log(crystal1v);
-console.log(crystal2v);
-console.log(crystal3v);
-console.log(crystal4v);
+// console.log(crystal1v);
+// console.log(crystal2v);
+// console.log(crystal3v);
+// console.log(crystal4v);
 $('#pic1').on('click', function(){
 
     let value1 = ($(this).attr("data-value1"));
     value1 = parseInt(value1);
     score += value1;
-    console.log(value1);
-    console.log(score);
+    // console.log(value1);
+    // console.log(score);
     $('#scoreW').text(score);
 });
 $('#pic2').on('click', function(){
 
-    let value2 = ($(this).attr("data-value2"));
+    let value2 = ($(this).attr("data-value1"));
     value2 = parseInt(value2);
     score += value2;
-    console.log(value2);
-    console.log(score);
+    // console.log(value2);
+    // console.log(score);
     $('#scoreW').text(score);
 });
 $('#pic3').on('click', function(){
 
-    let value3 = ($(this).attr("data-value3"));
+    let value3 = ($(this).attr("data-value1"));
     value3 = parseInt(value3);
     score += value3;
-    console.log(value3);
-    console.log(score);
+    // console.log(value3);
+    // console.log(score);
     $('#scoreW').text(score);
 });
 $('#pic4').on('click', function(){
 
-    let value4 = ($(this).attr("data-value4"));
+    let value4 = ($(this).attr("data-value1"));
     value4 = parseInt(value4);
     score += value4;
-    console.log(value4);
-    console.log(score);
+    // console.log(value4);
+    // console.log(score);
     $('#scoreW').text(score);
 });
-// $('#pic2').on('click', function(){ crystal2 + score});
-// $('#pic3').on('click', function(){ crystal3 + score});
-// $('#pic4').on('click', function(){ crystal4 + score});
+// conditional statements
 if (score > goal) {
     alert('You Lost');
     losses++;
 }
+console.log(losses);
 if (score === goal) {
     alert('you Win');
     wins++;
